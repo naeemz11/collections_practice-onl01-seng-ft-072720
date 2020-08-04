77d0426aac5c0dff05766fdbaebf56a2be62d640
Zannatul Naeem <mollahnaeem@gmail.com>
2020-08-04 00:21:38 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,57 @@
 
+def sort_array_asc(array)
+  array.sort do |a, b|
+    a <=> b 
+  end
+end 
+ 
+def sort_array_desc(array)
+  array.sort do |a,b|
+    b <=> a 
+  end 
+end 
+
+def sort_array_char_count(array)
+  array.sort do |a,b|
+    a.length <=> b.length
+  end 
+end 
+
+def swap_elements(array)
+  array.sort do |a,b|
+    a[1] <=> b[2]
+  end
+end 
+
+def reverse_array(array)
+  array.reverse
+end 
+
+def kesha_maker(array)
+array.each do |k|
+  k[2]="$"
+end
+end 
+
+def find_a(array)
+  array.find_all do |a|
+  a[0]=="a"
+end 
+end 
+
+def add_s(array)
+array.collect do |s|
+ if array[1]==s
+   s
+   else s + "s"
+   end 
+ end 
+end
+
+def sum_array(array)
+  sum= 0 
+  array.each do |add|
+  sum += add
+end 
+sum 
+end 
